bedbe088b7f23802b391aed2429f8fe80bd9eae2
liverpool09 <sohi.gursewak@gmail.com>
2021-10-05 23:00:33 +0530

    fix issues

--- js/index.js
+++ js/index.js
@@ -1,39 +1,42 @@
+$(document).ready(function() {
  // Swiper Hero
- var swiper = new Swiper("#hero-swiper", {
-     effect: "coverflow",
-     grabCursor: true,
-     centeredSlides: true,
-     slidesPerView: "auto",
-     speed: 800,
-     coverflowEffect: {
-         rotate: 50,
-         stretch: 0,
-         depth: 100,
-         modifier: 1,
-         slideShadows: true,
-     },
-     navigation: {
-         nextEl: '#nextSlide',
-         prevEl: '#prevSlide',
-     },
-     hashNavigation: {
-        watchState: true,
-      },
-
- });
-
-
-
-// Swiper Top Text
- var swiper2 = new Swiper(".hero-text-animations", {
-    direction: "vertical",
-    slidesPerView: 1,
-    loop: true,
-    simulateTouch:false,
-    autoplay: {
-        delay: 2000,
-      },
-  });
+ if($("#hero-swiper").length > 0) {
+    var swiper = new Swiper("#hero-swiper", {
+        effect: "coverflow",
+        grabCursor: true,
+        centeredSlides: true,
+        slidesPerView: "auto",
+        speed: 800,
+        coverflowEffect: {
+            rotate: 50,
+            stretch: 0,
+            depth: 100,
+            modifier: 1,
+            slideShadows: true,
+        },
+        navigation: {
+            nextEl: '#nextSlide',
+            prevEl: '#prevSlide',
+        },
+        hashNavigation: {
+           watchState: true,
+         },
+   
+    });
+  }
+ 
+  if($(".hero-text-animations").length > 0) {
+    // Swiper Top Text
+    var swiper2 = new Swiper(".hero-text-animations", {
+        direction: "vertical",
+        slidesPerView: 1,
+        loop: true,
+        simulateTouch:false,
+        autoplay: {
+            delay: 2000,
+        },
+    });
+  }
 
 
  // Lightbox Video
@@ -44,31 +47,49 @@
  });
 
 
-
-
- $(document).ready(function() {
-
      // Play video with Button
      $("#playButton").on("click ", () => {
          $("#about-video> a > img").trigger("click");
      });
+     
 
-     // Toggle Slide
-     $("#slide-two-view").hide();
-     swiper.on('slideChange', function() {
+      // Toggle Slide 
+     var hash = window.location.hash;  
+     if (hash) {
+        if (hash == "#hamavrally") {
+            showSecondSlide();
+         }
+         else {
+            showFirstSlide();
+         }
+     }
+
+
+     swiper.on('slideChange', function() { 
         new WOW().init();
-         if ($("#slide-one").hasClass("swiper-slide-active")) {
-             $("#slide-one-view").toggle();
-             $("#slide-two-view").toggle();
-             $(".first-slide-content").removeClass("active");
-             $(".second-slide-content").addClass("active");
-         } else {
-             $("#slide-one-view").toggle();
-             $("#slide-two-view").toggle();
-             $(".first-slide-content").addClass("active");
-             $(".second-slide-content").removeClass("active");
+     });
+     
+     swiper.on('hashChange', function() {
+        var changedHash = window.location.hash; 
+      
+        if (changedHash == "#hamavrally") {
+            showSecondSlide();
+         }
+         if (changedHash == "#yorunoyo") {
+            showFirstSlide();
          }
      });
-
-
+     function showFirstSlide () {
+        $("#slide-one-view").show();
+        $("#slide-two-view").hide();
+        $(".first-slide-content").addClass("active");
+        $(".second-slide-content").removeClass("active");
+       
+     }
+     function showSecondSlide () {
+        $("#slide-one-view").hide();
+        $("#slide-two-view").show();
+        $(".first-slide-content").removeClass("active");
+        $(".second-slide-content").addClass("active");    
+     }
  });
--- js/main.js
+++ js/main.js
@@ -1,25 +1,119 @@
-//Initiate WOW
-new WOW().init();
-// Go to top JS
-$(window).scroll(function() {
-  var scroll = $(window).scrollTop();
-  if (scroll >= 500) {
-    $(".go-to-top").addClass("show");
-  }
-  else {
-    $(".go-to-top").removeClass("show");
-  }
-}
-);
+$(document).ready(function() {
+    //Initiate WOW
+    new WOW().init();
 
-// On scroll Header  
-$(window).scroll(function() {
-  var scroll = $(window).scrollTop();
-  if (scroll >= 10) {
-    $(".navbar-floating").addClass("active");
-  }
-  else {
-    $(".navbar-floating").removeClass("active");
-  }
-}
-);
+    // Go to top JS
+    $(window).scroll(function() {
+        var scroll = $(window).scrollTop();
+        if (scroll >= 500) {
+            $(".go-to-top").addClass("show");
+        } else {
+            $(".go-to-top").removeClass("show");
+        }
+        if (scroll >= 10) {
+            $(".navbar-floating").addClass("active");
+        } else {
+            $(".navbar-floating").removeClass("active");
+        }
+    });
+
+    // Swiper Hero
+    if ($("#hero-swiper").length > 0) {
+        var swiper = new Swiper("#hero-swiper", {
+            effect: "coverflow",
+            grabCursor: true,
+            centeredSlides: true,
+            slidesPerView: "auto",
+            speed: 800,
+            coverflowEffect: {
+                rotate: 50,
+                stretch: 0,
+                depth: 100,
+                modifier: 1,
+                slideShadows: true,
+            },
+            navigation: {
+                nextEl: '#nextSlide',
+                prevEl: '#prevSlide',
+            },
+            hashNavigation: {
+                watchState: true,
+            },
+        });
+        // Toggle Slide 
+        var hash = window.location.hash;
+        if (hash) {
+            if (hash == "#hamavrally") {
+                showSecondSlide();
+            } else {
+                showFirstSlide();
+            }
+        }
+
+        swiper.on('slideChange', function() {
+            new WOW().init();
+            window.scrollTo(0, 0);
+        });
+
+        swiper.on('hashChange', function() {
+            var changedHash = window.location.hash;
+            if (changedHash == "#hamavrally") {
+                showSecondSlide();
+            }
+            if (changedHash == "#yorunoyo") {
+                showFirstSlide();
+            }
+        });
+
+        function showFirstSlide() {
+            $("#slide-one-view").show();
+            $("#slide-two-view").hide();
+            $(".first-slide-content").addClass("active");
+            $(".second-slide-content").removeClass("active");
+
+        }
+
+        function showSecondSlide() {
+            $("#slide-one-view").hide();
+            $("#slide-two-view").show();
+            $(".first-slide-content").removeClass("active");
+            $(".second-slide-content").addClass("active");
+        }
+
+    }
+
+    if ($(".hero-text-animations").length > 0) {
+        // Swiper Top Text
+        var swiper2 = new Swiper(".hero-text-animations", {
+            direction: "vertical",
+            slidesPerView: 1,
+            loop: true,
+            simulateTouch: false,
+            autoplay: {
+                delay: 2000,
+            },
+        });
+    }
+
+
+
+    var gallery = document.querySelectorAll('.video-gallery');
+
+    if (gallery.length) {
+        for (var i = 0; i < gallery.length; i++) {
+            lightGallery(gallery[i], {
+                plugins: [lgVideo],
+                autoplayVideoOnSlide: true,
+                zoom: true
+            });
+        }
+    }
+    if ($(".play-video").length > 0) {
+        $(".play-video").click(function() {
+            $(this).closest(".video-main-block").find(".video-gallery img").trigger("click");
+        });
+
+    }
+
+
+});
